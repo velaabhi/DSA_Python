b4c242544222abc73472beb35562a810337b889d
abhi <velaabhi@gmail.com>
2025-12-25 09:03:35 +0530

    Implemented creation of list by append and extend by head and tail recursion respectively

--- 02_Data Structures/Section_24_Recursion/005_print_1_to_n_by_recursion.py
+++ 02_Data Structures/Section_24_Recursion/005_print_1_to_n_by_recursion.py
@@ -14,3 +14,27 @@ def print_1_to_n(n):
 
 n = int(input("Enter n "))
 print_1_to_n(n)
+
+# 2nd Implementation  of generation of list by using recursion
+
+def count_to_n(n):
+    """
+    Function to return a list of integers from 1 to n using recursion.
+
+    Parameters:
+    n (int): The positive integer representing the upper limit of the range.
+
+    Returns:
+    list: A list of integers from 1 to n.
+    """
+    # Your code here
+    if n == 0:  # base cond
+        return []
+
+    result = count_to_n(n - 1)  # head recursion
+    result.append(n)
+    return result
+
+
+n = 5
+print(count_to_n(n))
